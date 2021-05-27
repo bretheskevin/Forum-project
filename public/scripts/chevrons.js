5949d37ee9371aec589110a9b910136416631732
Kévin Brèthes <bretheskevin@gmail.com>
2021-05-27 16:58:58 +0200

    chevrons

---
+++ public/scripts/chevrons.js
@@ -0,0 +1,20 @@
+const chevrons = document.body.getElementsByClassName("chevrons");
+const triggers = document.body.getElementsByClassName("collapsible-trigger");
+
+for (const trigger of triggers) {
+    trigger.addEventListener("click", () => {
+        for (const chevron of chevrons) {
+            if (trigger.lastElementChild === chevron) {
+                if (chevron.classList.contains("mdi-chevron-down")) {
+                    chevron.classList.remove("mdi-chevron-down");
+                    chevron.classList.add("mdi-chevron-up");
+                } else {
+                    chevron.classList.add("mdi-chevron-down");
+                    chevron.classList.remove("mdi-chevron-up");
+                }
+            }
+        }
+    })
+
+    trigger.click();
+}
