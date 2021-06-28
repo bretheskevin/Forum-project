953f739aad0e212d03539af1bee4ff872f0700e3
THOLLINGWORTH <tom.hollingworh@ynov.com>
2021-06-28 12:24:54 +0200

    added google auth

---
+++ auth/google/base.go
@@ -0,0 +1,13 @@
+//package google
+
+//import (
+//	"net/http"
+//)
+
+//func New() http.Handler {
+//	mux.Handle("/",  http.FileServer(http.Dir("templates/")))
+
+//	mux.HandleFunc("/auth/google/callback", oauthGoogleCallback)
+
+//	return mux
+//}
